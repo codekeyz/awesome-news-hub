6405cf43d00186a57b660c1cbb9c92273bb76cb7
Precious Keyz <ositachima.co@gmail.com>
2019-03-24 14:40:16 -0700

    Updated Rest-API

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 from gharage import GharageNewsClient
 
 app = Flask(__name__)
+client = GharageNewsClient()
+
 
 @app.route("/")
 def hello():
-  news = GharageNewsClient().get_news()
-  resp = Response(news, status=200, mimetype='application/json')
-  return resp
+    # Getting the page number from the request
+    pageNumber = request.args.get('page')
+
+    if pageNumber is not None and int(pageNumber) is not None:
+        news = client.get_news(page=pageNumber)
+    else:
+        news = client.get_news()
+    resp = Response(news, status=200, mimetype='application/json')
+    return resp
+
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
