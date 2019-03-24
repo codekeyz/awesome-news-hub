1e0dc029c83f8cf3a9ef92435a415286b23b8b31
Precious Keyz <ositachima.co@gmail.com>
2019-03-23 17:44:58 -0700

    Refactored API

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 from gharage import GharageNewsClient
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return GharageNewsClient().get_news()
+  news = GharageNewsClient().get_news()
+  resp = jsonify(news)
+  return resp
 
 if __name__ == "__main__":
   app.run()
